e6aebe97807ec35144f253bbc3ba754c29f600f8
iaalharbi <alharbi.ibrahim@gmail.com>
2020-06-01 14:04:41 +0300

    for loop

---
+++ forloop.py
@@ -0,0 +1,10 @@
+subjects = ["Math","History","Accounting"]
+
+# print(subjects[0])
+subjects.append("history")
+# print(subjects)
+# print(subjects[-1])
+# subjects.pop(1)
+# print(subjects)
+for x in subjects:
+    print(x)
