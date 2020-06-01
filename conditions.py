944d75d127896d8a82d07db1f3cdbe0ea719caa4
iaalharbi <alharbi.ibrahim@gmail.com>
2020-06-01 13:16:45 +0300

    condition

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input("Please type your username: ")
+password = input("Please type your password: ")
+if (username == "iaalharbi" and password == "123456"):
+    print("Welcome to Gmail")
+else:
+    print("username or password not correct")
